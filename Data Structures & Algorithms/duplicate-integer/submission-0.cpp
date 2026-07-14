feb8930c84976b1aaea6d64bff23d1f1cac33071
Sai Charan Mahankali <mahankalisaicharan@gmail.com>
2026-07-14 07:28:31 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for(auto num:nums){
+
+            if(seen.count(num)==0){
+
+                seen.insert(num);
+            }else{
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
