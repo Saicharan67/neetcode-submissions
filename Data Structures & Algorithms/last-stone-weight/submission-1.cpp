8e6f1595161758af4ffb41826441bd9c99389724
Sai Charan Mahankali <mahankalisaicharan@gmail.com>
2026-07-21 08:08:59 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        priority_queue<int> pq;
+        for(auto s:stones){
+            pq.push(s);
+        }
+
+        while(pq.size()>1){
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            cout<<x;
+            cout<<y;
+            cout<<pq.size();
+            if(x!=y){
+                pq.push(abs(x-y));
+            }
+        }
+        
+
+        return pq.size()?pq.top():0;
+    }
+};
