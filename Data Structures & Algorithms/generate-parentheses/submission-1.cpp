c42dafa7ece92bab69f89b10b9dc3f818a1950f8
Sai Charan Mahankali <mahankalisaicharan@gmail.com>
2026-08-11 08:27:02 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    void backtrack(vector<string> &res,string curr,int open,int close,int n){
+       
+        if(curr.length()==2*n){
+            res.push_back(curr);
+            return;
+        }
+
+        if(open<n){
+            curr+="(";
+            backtrack(res,curr,open+1,close,n);
+            curr.pop_back();
+        }
+        if(close<open){
+            curr+=")";
+            backtrack(res,curr,open,close+1,n);
+            curr.pop_back();
+        }      
+
+       
+    }
+    vector<string> generateParenthesis(int n) {
+
+
+        vector<string> res;
+        string curr = "";
+        backtrack(res,curr,0,0,n);
+        return res;
+        
+    }
+};
